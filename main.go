04c20879d551a175f3b7677e337598e73a146668
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-08-10 10:03:21 +0900

    use `options.Quality` for jpeg encoding

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ type input struct {
 	dst struct {
 		width       int
 		height      int
+		quality     int
 		contentType string
 		w           io.Writer
 	}
@@ -59,6 +60,19 @@ func parse(w http.ResponseWriter, r *http.Request) (*input, error) {
 		}
 	}
 
+	qualityStr := query.Get("quality")
+	quality := 0
+
+	if qualityStr != "" {
+		var err error
+
+		quality, err = strconv.Atoi(qualityStr)
+
+		if err != nil {
+			return &input{}, err
+		}
+	}
+
 	return &input{
 		src: struct {
 			contentType string
@@ -70,12 +84,14 @@ func parse(w http.ResponseWriter, r *http.Request) (*input, error) {
 		dst: struct {
 			width       int
 			height      int
+			quality     int
 			contentType string
 			w           io.Writer
 		}{
 			contentType: r.Header.Get("accept"),
 			width:       width,
 			height:      height,
+			quality:     quality,
 			w:           w,
 		},
 	}, nil
@@ -136,7 +152,15 @@ func execute(input *input) error {
 			return err
 		}
 	case "image/jpeg":
-		if err := jpeg.Encode(input.dst.w, dst, nil); err != nil {
+		options := jpeg.Options{
+			Quality: 100,
+		}
+
+		if input.dst.quality != 0 {
+			options.Quality = input.dst.quality
+		}
+
+		if err := jpeg.Encode(input.dst.w, dst, &options); err != nil {
 			return err
 		}
 	case "image/gif":
