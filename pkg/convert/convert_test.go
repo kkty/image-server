dbcae0510a35470e26c9abae6d872b6865c13924
Kazushi Kitaya <kazushikitaya@gmail.com>
2019-09-24 08:54:00 +0900

    add tests for `convert.ParseHTTPRequest`

--- pkg/convert/convert_test.go
+++ pkg/convert/convert_test.go
@@ -6,9 +6,79 @@ import (
 	"image/gif"
 	"image/jpeg"
 	"image/png"
+	"net/http"
+	"net/url"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func testParseHTTPRequest(
+	t *testing.T,
+	query url.Values,
+	header http.Header,
+	assertFn func(t *testing.T, config *Config),
+) {
+	req, err := http.NewRequest("POST", "http://example.com/", &bytes.Buffer{})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.URL.RawQuery = query.Encode()
+	req.Header = header
+
+	config, err := ParseHTTPRequest(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertFn(t, config)
+}
+
+func TestParseHTTPRequest(t *testing.T) {
+	t.Run("WithoutQuery", func(t *testing.T) {
+		testParseHTTPRequest(
+			t,
+			url.Values{},
+			http.Header{
+				"Content-Type": []string{"image/png"},
+				"Accept":       []string{"image/jpeg"},
+			},
+			func(t *testing.T, config *Config) {
+				assert.Equal(t, "image/png", config.ContentTypeFrom)
+				assert.Equal(t, "image/jpeg", config.ContentTypeTo)
+				assert.Equal(t, 0, config.Width)
+				assert.Equal(t, 0, config.Height)
+				assert.Equal(t, 0, config.Quality)
+			},
+		)
+	})
+
+	t.Run("WithQuery", func(t *testing.T) {
+		testParseHTTPRequest(
+			t,
+			url.Values{
+				"width":   []string{"100"},
+				"height":  []string{"50"},
+				"quality": []string{"30"},
+			},
+			http.Header{
+				"Content-Type": []string{"image/png"},
+				"Accept":       []string{"image/jpeg"},
+			},
+			func(t *testing.T, config *Config) {
+				assert.Equal(t, "image/png", config.ContentTypeFrom)
+				assert.Equal(t, "image/jpeg", config.ContentTypeTo)
+				assert.Equal(t, 100, config.Width)
+				assert.Equal(t, 50, config.Height)
+				assert.Equal(t, 30, config.Quality)
+			},
+		)
+	})
+}
 
 func TestConvert(t *testing.T) {
 	srcs := []struct {
